fe08a77d2ea5ec3f9e63352d1c73127d3de91644
imsunying <imsunying@hotmail.com>
2017-03-07 20:20:26 -0500

    feature change
    generate the time randomly, and add comment for classes

--- carGame/Car.java
+++ carGame/Car.java
@@ -1,56 +1,70 @@
-package carGame;
-
+/**
+ * 
+ * @author Ying Sun
+ * The Car class contains the information of a car required, which including carPath,speed,time,name and winner 
+ *
+ */
 public class Car {
 	private CarPath carPath;
 	private boolean winner;
-	private int speed;
-	private double time;
+	private double speed;
+	private int time;
 	private String name;
+	
+	//Car's constructor
 	public Car(String name, CarPath carPath) {
 		this.name = name;
-		this.speed = generateSpeed();
+		//generate the time randomly.
+		this.time = generateTime();
 		this.winner = false;
 		this.carPath = carPath;
 	}
+	
+	//getter and setter method
 	public CarPath getCarPath() {
 		return carPath;
 	}
+	
 	public void setCarPath(CarPath carPath) {
 		this.carPath = carPath;
 	}
+	
 	public boolean isWinner() {
 		return winner;
 	}
+	
 	public void setWinner(boolean winner) {
 		this.winner = winner;
 	}
-	public int getSpeed() {
-		return speed;
-	}
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-	public double getTime() {
-		double time = 0;
+	
+	//Calucalte the speed based on the time
+	public double getSpeed() {
+		int totalDistance = 0;
 		Stop[] stops = carPath.getStops();
 		for (int i = 0; i < stops.length; i++) {
-			time += stops[i].getDistance() / this.getSpeed();
+			totalDistance += stops[i].getDistance();
 		}
+		return totalDistance / this.getTime();
+	}
+	
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	
+	public double getTime() {
 		return time;
 	}
+	
 	public void setTime(int time) {
 		this.time = time;
 	}
 	
-	public int generateSpeed() {
-		return (int) Math.ceil(Math.random() * 1000 + 1); 
+	//randomly generate the time
+	public int generateTime() {
+		return (int) Math.ceil(Math.random() * 100 + 1); 
 	}
 	
 	public String getName() {
 		return name;
 	}
-	public void go() {
-		
-	}
-	
 }
--- carGame/Stop.java
+++ carGame/Stop.java
@@ -1,35 +1,48 @@
-package carGame;
-
+/**
+ * 
+ * @author Ying Sun
+ * Stop class describes that the stop information which includes name, distance etc
+ */
 public class Stop {
 	private String name;
 	private int x;
 	private int y;
+	//each stop has next stop
 	private Stop next;
 	private int Distance;
+	
 	public Stop(String name, int distance) {
 		this.name = name;
 		this.Distance = distance;
 	}
+	//getter and setter
 	public String getName() {
 		return name;
 	}
+	
 	public int getX() {
 		return x;
 	}
+	
 	public int getY() {
 		return y;
 	}
+	
 	public Stop getNext() {
 		return next;
 	}
+	
 	public void setNext(Stop next) {
 		this.next = next;
 	}
+	
 	public int getDistance() {
 		return Distance;
 	}
+	
 	public void setDistance(int distance) {
 		Distance = distance;
 	}
 	
 }
+
